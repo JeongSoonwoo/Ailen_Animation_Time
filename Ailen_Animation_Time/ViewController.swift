620c99f76ec75eb77d1d4c423fb6cd725c0180e7
정순우 <soonwoo2714@gmail.com>
2018-04-05 15:29:26 +0900

    toggle switch

--- Ailen_Animation_Time/ViewController.swift
+++ Ailen_Animation_Time/ViewController.swift
@@ -9,11 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var counter = 1
+    var timer = Timer()
+    var direction = true
+    var playing = true
 
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = String(counter)
+        image.image = UIImage(named: "frame1.png")
     }
+    @IBAction func start(_ sender: Any) {
+        
+        if playing == false {
+            timer.invalidate()
+            playing = true
+        
+        } else if playing == true {
+              timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.doAnimation), userInfo: nil, repeats: true)
+            playing = false
+        }
+    }
+    @IBAction func stop(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @objc func doAnimation() {
+        if counter == 5 {
+            direction = false
+        }else if counter == 1 {
+            direction = true
+        }
+        
+        if direction == true {
+            counter = counter + 1
+        } else if direction == false {
+            counter = counter - 1
+        }
+        
+        image.image = UIImage(named: "frame\(counter).png")
+        label.text = String(counter)
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
